02a4bbd11790da25bc283ea79f6f911ab827c3de
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-04-26 14:58:56 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {Header} from './components/Header';
+import { Post } from "./Post";
+import './global.css';
+
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
+export function App() {
+  return (
+    <div>
+      <Header/>
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post author="Gabriel" content="Lorem Ipsum"/>
+          <Post author="Gabriel" content="Lorem Ipsum"/>
+        </main>
+        </div>
+    </div>
+  );
+}
